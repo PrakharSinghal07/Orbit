d251b7b39c841a9fb903bab71c5566ff4089ad11
NEMYSESx <shakirraza4227@gmail.com>
2025-04-01 20:42:33 +0530

    added a docker compose and a config file for the kafka and kafka connector to ingest the files

--- main.go
+++ main.go
@@ -1,35 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	// "fmt"
+	// "os"
+	// "os/signal"
+	// "syscall"
+	// "time"
 
-	"github.com/NEMYSESx/orbit/apps/data/logs"
+	// "github.com/NEMYSESx/orbit/apps/data/logs"
+	tika_extractor "github.com/NEMYSESx/orbit/apps/ingestion-pipeline/cmd/tika-extractor"
 )
 
 func main() {
-	config := logs.DefaultLogConfig()
+	// config := logs.DefaultLogConfig()
 	
-	config.OutputDir = "./synthetic_logs"
-	config.LogsPerMinute = map[string]int{
-		"system":  20,  
-		"network": 10,  
-		"cluster": 5,   
-		"slurm":   15,  
-	}
-	config.RotationInterval = 1 * time.Hour  
+	// config.OutputDir = "./synthetic_logs"
+	// config.LogsPerMinute = map[string]int{
+	// 	"system":  20,  
+	// 	"network": 10,  
+	// 	"cluster": 5,   
+	// 	"slurm":   15,  
+	// }
+	// config.RotationInterval = 1 * time.Hour  
 	
 	
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	// sigChan := make(chan os.Signal, 1)
+	// signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 	
-	go logs.LogsWithConfig(config)
+	// go logs.LogsWithConfig(config)
 	
-	fmt.Println("Log generator started. Press Ctrl+C to stop.")
+	// fmt.Println("Log generator started. Press Ctrl+C to stop.")
 	
-	<-sigChan
-	fmt.Println("\nShutting down log generator...")
+	// <-sigChan
+	// fmt.Println("\nShutting down log generator...")
+
+	tika_extractor.TikaExtractor()
 }
